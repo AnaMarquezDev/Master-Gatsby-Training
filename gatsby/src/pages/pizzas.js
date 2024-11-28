f5741d4e30715591a452e835e9a9954afae150e1
AnaMarquezDev <Ana_Marquez@trimble.com>
2024-11-27 16:39:09 -0800

    Getting Data Into Gatsby with GraphQL

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import Nav from '../components/Nav';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <Nav />
-      <p>Hey! I'm the Pizzas page</p>
+      <PizzaList pizzas={pizzas} />
+      <p>Hey! There are {pizzas.length} Pizzas!!</p>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fixed(width: 200, height: 200) {
+              ...GatsbySanityImageFixed
+            }
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
